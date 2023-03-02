c8638757ee0b9f1ac785f8091e6fa4450b3fe409
buraksb10 <burakberzener@gmail.com>
2023-03-02 23:35:32 +0300

    comments added

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,15 @@
 Tasks:
 */
 
-int distance_front_left, distance_front_right, distance_side_left, distance_side_right, distance_back;
-int counter_front_left = 0, counter_front_right=0, counter_side_left=0, counter_side_right=0, counter_back=0;
-int rand_number = 0;
-int sensor_checker_status;
-int counter = 0;
-int random_movement;
+ /*Private Variables BEGIN*/
+
+ /*Private Variables END*/
+
+ /*Private Functions Define BEGIN*/
+
+ /*Private Functions Define END*/
 
 espnow_msg_struct espnow_msg_receive;
-
-int sensor_checker(int wait_time);
-void getSensorDistances();
 
 //callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
@@ -94,237 +92,11 @@ void loop() {
   Serial.println("******MAIN LOOP******");
 
   if(espnow_msg_receive.data == 'o') {
-  getSensorDistances();
-  rand_number=random(0, 100);
+    /*Algo Code BEGIN*/
 
-  //Not Recommended Stage
-  /*
-  while(distance_front_right>=400) {
-    getSensorDistances();
-    moveMotors(ROTATE_ON_LEFT_BWD);
-    delay(50);
-  }
-  while(distance_front_left>=400) {
-    getSensorDistances();
-    moveMotors(ROTATE_ON_RIGHT_BWD);
-    delay(50);
-  }
-  */
- 
-  //First Stage
-  if(distance_front_right<=100 && distance_front_left<=100) {
-    if(distance_back>=100) {
-      while(distance_front_right<=110 || distance_front_left<=110) {
-        getSensorDistances();
-        moveMotors(GO_BACKWARD);
-        delay(100);
-      }
-    }
-    else {
-      if(distance_side_left<=110) {
-          moveMotors(TURN_LEFT);
-          delay(400);
-          moveMotors(GO_FORWARD);
-          delay(400);
-      }
-      else if(distance_side_right<=110) {
-          moveMotors(TURN_RIGHT);
-          delay(400);
-          moveMotors(GO_FORWARD);
-          delay(400);
-      }
-    }
-  }
-  else if(distance_front_right<=100) {
-    if(distance_front_left<=100) {
-      getSensorDistances();
-      moveMotors(GO_BACKWARD);
-      delay(100);
-    }
-    if(distance_front_right>=60) {
-      getSensorDistances();
-      moveMotors(GO_BACKWARD);
-      delay(100);
-      moveMotors(ROTATE_ON_LEFT_BWD);
-      delay(100);
-      moveMotors(GO_FORWARD);
-      delay(100);
-      while(distance_side_right<FRONT_TRIG_SIDE_CTRL) {
-        getSensorDistances();
-        moveMotors(ROTATE_ON_LEFT_BWD);
-        delay(400);
-        moveMotors(GO_BACKWARD);
-        delay(400);
-      }
-    }
-    else {
-      while (distance_front_right<80) {
-        getSensorDistances();
-        moveMotors(ROTATE_ON_LEFT_BWD);
-        delay(400);
-        moveMotors(GO_BACKWARD);
-        delay(400);
-      }
-    }
-  }
-  else if(distance_front_left<=100) {
-    if(distance_front_right<=100) {
-      getSensorDistances();
-      moveMotors(GO_BACKWARD);
-      delay(100);
-    }
-    if(distance_front_left>=60) {
-      getSensorDistances();
-      moveMotors(ROTATE_ON_LEFT_BWD);
-      delay(100);
-      moveMotors(GO_BACKWARD);
-      delay(100);
-      moveMotors(GO_FORWARD);
-      delay(100);
-      while (distance_side_left<FRONT_TRIG_SIDE_CTRL) {
-        getSensorDistances();
-        moveMotors(ROTATE_ON_RIGHT_BWD);
-        delay(400);
-        moveMotors(GO_BACKWARD);
-        delay(400);
-      }
-    }
-    else {
-      while (distance_front_left<80) {
-        getSensorDistances();
-        moveMotors(ROTATE_ON_RIGHT_BWD);
-        delay(400);
-        moveMotors(GO_BACKWARD);
-        delay(400);
-      }
-    }
-  }
-  
-
-  //Second Stage
-    if((distance_front_left>=120 && distance_front_right>=120) && rand_number>=50) {
-      int i = 0;
-      if(rand_number>80) {
-        while(i>=50) {
-          getSensorDistances();
-          moveMotors(GO_FORWARD);
-          delay(20);
-          i++;
-        }
-      }
-      else if(rand_number<60 && rand_number>55) {
-        while(i>=80) {
-          getSensorDistances();
-          moveMotors(TURN_LEFT);
-          delay(20);
-          i++;
-        }
-      }
-      else if(rand_number<55 && rand_number>50) {
-        while(i>=80) {
-          getSensorDistances();
-          moveMotors(TURN_RIGHT);
-          delay(20);
-          i++;
-        }
-      }
-    }
-    else if(distance_back>100 && (rand_number<=50 && rand_number > 30)) {
-        int i=0;
-        while(i>=80) {
-          getSensorDistances();
-          moveMotors(GO_BACKWARD);
-          delay(20);
-          i++;
-        }
-    }
-    else if(distance_side_left>150 && rand_number > 15) {
-        if(rand_number>22) {
-          random_movement = ROTATE_ON_RIGHT_BWD;
-        }
-        else {
-          random_movement = ROTATE_ON_LEFT_FWD;
-        }
-        int i = 0;
-        while(i>=100) {
-          getSensorDistances();
-          moveMotors(random_movement);
-          delay(20);
-          i++;
-        }
-    }
-    if(distance_side_right>150 && rand_number > 0) {
-        if(rand_number>7) {
-          random_movement = ROTATE_ON_RIGHT_FWD;
-        }
-        else {
-          random_movement = ROTATE_ON_LEFT_BWD;
-        }
-        int i = 0;
-        while(i>=100) {
-          getSensorDistances();
-          moveMotors(random_movement);
-          delay(20);
-          i++;
-        }
-    }
+    /*Algo Code END*/
   }
 }
+ /*Private Functions BEGIN*/
 
-void getSensorDistances() {
-  distance_front_right = getDistance(sensorFrontRightTrig, sensorFrontRightEcho);
-  distance_front_left = getDistance(sensorFrontLeftTrig, sensorFrontLeftEcho);
-  distance_side_right = getDistance(sensorSideRightTrig, sensorSideRightEcho);
-  distance_side_left = getDistance(sensorSideLeftTrig, sensorSideLeftEcho);
-  distance_back = getDistance(sensorBackTrig, sensorBackEcho); 
-
-  #if _DEBUGMODE
-    Serial.print("cm front right: ");
-    Serial.print(distance_front_right);
-    Serial.print("|   cm front left: ");
-    Serial.print(distance_front_left);
-    Serial.print("|   cm side right: ");
-    Serial.print(distance_side_right);
-    Serial.print("|   cm side left: ");
-    Serial.print(distance_side_left);
-    Serial.print("|   cm back: ");
-    Serial.println(distance_back);
-    Serial.print("Random number: ");
-    Serial.println(rand_number);
-  #endif
-
-  if(distance_front_right<CRICITAL_DISTANCE) {
-    counter=0;
-    moveMotors(TURN_RIGHT);
-    delay(TURNING_ON_TWO_DELAY);
-    moveMotors(GO_BACKWARD);
-    delay(400);
-    counter_front_right++;
-  }
-  if(distance_front_left<CRICITAL_DISTANCE) {
-    counter=0;
-    moveMotors(TURN_LEFT);
-    delay(TURNING_ON_TWO_DELAY);
-    moveMotors(GO_BACKWARD);
-    delay(BACKWARD_DELAY);
-    counter_front_left++;
-  }
-  if(distance_side_right<CRICITAL_DISTANCE) {
-    counter=0;
-    moveMotors(ROTATE_ON_RIGHT_FWD);
-    delay(TURNING_ON_ONE_DELAY);
-    counter_side_right++;
-  }
-  if(distance_side_left<CRICITAL_DISTANCE) {
-    counter=0;
-    moveMotors(ROTATE_ON_LEFT_FWD);
-    delay(TURNING_ON_ONE_DELAY);
-    counter_side_left++;
-  }
-  if(distance_back<CRICITAL_DISTANCE) {
-    counter=0;
-    moveMotors(GO_FORWARD);
-    delay(FORWARD_DELAY);
-    counter_back++;
-  }
-}
+ /*Private Functions END*/
